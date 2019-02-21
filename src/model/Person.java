f0a1e93fe6ceb0dce0ae88ebccf1e0d923bbbce2
yeland <wlx100715@163.com>
2019-02-21 21:45:01 +0800

    Add constructor of class Person.

--- src/model/Person.java
+++ src/model/Person.java
@@ -5,6 +5,12 @@ public class Person {
     private String name;
     private int age;
 
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
     public int getId() {
         return this.id;
     }
