3f72f59282eec7e0897b8a63698abd31deb2040f
yeland <wlx100715@163.com>
2019-02-21 21:39:53 +0800

    Add the class of person which will introduce.

---
+++ src/model/Person.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is" + this.name + ".I am" + this.age + "years old.";
+    }
+}
