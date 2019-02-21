56cb49576b39b901ee1c45c68547eb280f993d55
yeland <wlx100715@163.com>
2019-02-21 21:52:50 +0800

    Student has class and its own function.

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,16 @@
 package model;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+    public Student(int id, String name, int age, int klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+"I am at class"+klass+".";
+    }
 }
+
